38878d47e590b235b96773c286fb75e87c85acb7
Samadar1 <Urapol18@gmail.com>
2026-02-09 18:02:11 +0300

    first commit

---
+++ src/test/java/com/shoriext/auth_service/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoriext.auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
