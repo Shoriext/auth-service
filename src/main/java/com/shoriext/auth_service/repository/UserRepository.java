112f4d9fc7c563a0bb0759eb2bd70fbfce1d3e6e
Samadar1 <Urapol18@gmail.com>
2026-02-09 18:47:10 +0300

    add: UserRepository

---
+++ src/main/java/com/shoriext/auth_service/repository/UserRepository.java
@@ -0,0 +1,20 @@
+package com.shoriext.auth_service.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.shoriext.auth_service.model.entity.User;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByUsername(String username);
+
+    boolean existsByEmail(String email);
+
+    boolean existsByUsername(String username);
+}
